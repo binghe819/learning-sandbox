8db275766de3daf1d0ab00db63313d0cccf25c9e
binghe819 <binghe819@gmail.com>
2021-12-03 17:10:23 +0900

    init: JPA N + 1 문제 학습 테스트 초기 설정

---
+++ jpa-n-plus-1-basic/src/main/java/com/binghe/jpanplus1/JpaNPlus1Application.java
@@ -0,0 +1,13 @@
+package com.binghe.jpanplus1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNPlus1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaNPlus1Application.class, args);
+    }
+
+}
---
+++ jpa-n-plus-1-basic/src/test/java/com/binghe/jpanplus1/JpaNPlus1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.jpanplus1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNPlus1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
