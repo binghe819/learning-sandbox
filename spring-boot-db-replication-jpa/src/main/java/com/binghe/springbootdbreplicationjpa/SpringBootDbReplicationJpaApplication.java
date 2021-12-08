875fc7e23201db2c0f4e55b45318e81e51373f0a
binghe819 <binghe819@gmail.com>
2021-12-08 17:15:19 +0900

    [DB Replication JPA : init] Spring Boot 기반 DB Replication JPA 초기 설정

---
+++ spring-boot-db-replication-jpa/src/main/java/com/binghe/springbootdbreplicationjpa/SpringBootDbReplicationJpaApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.springbootdbreplicationjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDbReplicationJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDbReplicationJpaApplication.class, args);
+    }
+
+}
---
+++ spring-boot-db-replication-jpa/src/test/java/com/binghe/springbootdbreplicationjpa/SpringBootDbReplicationJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.springbootdbreplicationjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDbReplicationJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
