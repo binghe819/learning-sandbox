85f1f90ff8f0e3ec535f1b0d39889d9ccee20932
binghe819 <binghe819@gmail.com>
2021-12-15 20:58:51 +0900

    [Query Counter : init] Spring Boot + JPA 기반 Query Counter 초기 설정

---
+++ spring-jpa-query-counter/src/test/java/com/binghe/springjpaquerycounter/SpringJpaQueryCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.springjpaquerycounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaQueryCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
