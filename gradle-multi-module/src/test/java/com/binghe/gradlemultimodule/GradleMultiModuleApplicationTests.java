46cd95013a8f52f32a5b2f4b18e057cc3084e0f7
binghe819 <binghe819@gmail.com>
2022-01-23 23:07:58 +0900

    [gradle-multi-module : init] Gradle 멀티 모듈 프로젝트 초기 설정

---
+++ gradle-multi-module/src/test/java/com/binghe/gradlemultimodule/GradleMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.gradlemultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleMultiModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
