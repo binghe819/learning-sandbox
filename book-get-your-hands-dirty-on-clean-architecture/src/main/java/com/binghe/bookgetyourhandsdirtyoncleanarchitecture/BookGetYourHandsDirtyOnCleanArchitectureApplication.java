baad7c9590f266ad2a4d9f8852daf6c688ef62b9
김병화 <binghe819@woowahan.com>
2022-02-13 20:26:09 +0900

    [book-clean-architecture-practice : init] 만들면서 배우는 클린 아키텍처 초기 설정

---
+++ book-get-your-hands-dirty-on-clean-architecture/src/main/java/com/binghe/bookgetyourhandsdirtyoncleanarchitecture/BookGetYourHandsDirtyOnCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.bookgetyourhandsdirtyoncleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookGetYourHandsDirtyOnCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookGetYourHandsDirtyOnCleanArchitectureApplication.class, args);
+    }
+
+}
