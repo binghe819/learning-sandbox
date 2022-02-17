50c01eb00a933eeeb23f58a03a73394b0d154cf0
김병화 <binghe819@woowahan.com>
2022-02-18 00:16:11 +0900

    [kotlin-hello-world : IO] 입출력 Hello World

---
+++ kotlin-hello-world/src/main/kotlin/example/input_output/InputOutputExample.kt
@@ -0,0 +1,11 @@
+package example.input_output
+
+fun main() {
+    print("이름 > ")
+    val name = readLine();
+
+    print("나이 > ")
+    val age = readLine() !!.toInt();
+
+    println("입력한 이름: $name, 입력한 나이: $age");
+}
