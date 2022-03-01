49ea4819a740ef7ee175392054136949435efb76
binghe819 <binghe819@gmail.com>
2022-03-01 17:34:35 +0900

    [quick-commerce-read-before: init] 퀵커머스 도메인 조회 파일럿 프로젝트 생성

---
+++ quick-commerce-read-before/src/main/java/com/binghe/quickcommercereadbefore/QuickCommerceReadBeforeApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.quickcommercereadbefore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickCommerceReadBeforeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickCommerceReadBeforeApplication.class, args);
+    }
+
+}
---
+++ quick-commerce-read-before/src/test/java/com/binghe/quickcommercereadbefore/QuickCommerceReadBeforeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.quickcommercereadbefore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickCommerceReadBeforeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
