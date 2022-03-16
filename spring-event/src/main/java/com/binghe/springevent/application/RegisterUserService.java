26a105d260fe19727d5078d3c1e47d636d8e3a05
binghe819 <binghe819@gmail.com>
2022-03-17 03:21:24 +0900

    [spring-event : learning test] 이벤트 적용 후 비동기 및 트랜잭션 문제 해결

--- spring-event/src/main/java/com/binghe/springevent/application/RegisterUserService.java
+++ spring-event/src/main/java/com/binghe/springevent/application/RegisterUserService.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
+@Transactional
 @Service
 public class RegisterUserService {
 
@@ -15,10 +17,12 @@ public class RegisterUserService {
 
     public void register(RegisterUserCommand registerUserCommand) {
         // 회원 가입
-        System.out.println(registerUserCommand.getName() + "님 회원 가입 완료 - DB에 영속화");
+        System.out.println(registerUserCommand.getName() + "님 회원 가입 요청 - DB에 영속화");
 
         // 가입 완료 이벤트 발행
         publisher.publishEvent(new RegisteredUserEvent(registerUserCommand.getName()));
+
+        throw new RuntimeException("일부러 예외 터트림");
 //        System.out.println(registerUserCommand.getName() + "님 가입 축하합니다. 쿠폰 번호: xxxxx");
 //        System.out.println(registerUserCommand.getName() + "님 가입 축하합니다. 문자");
     }
--- spring-event/src/main/java/com/binghe/springevent/event/EventHandler.java
+++ spring-event/src/main/java/com/binghe/springevent/event/EventHandler.java
@@ -1,18 +1,25 @@
 package com.binghe.springevent.event;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class EventHandler {
 
-    @EventListener
-    public void sendEmail(RegisteredUserEvent event) {
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void sendEmail(RegisteredUserEvent event) throws InterruptedException {
+        Thread.sleep(3_000);
         System.out.println(event.getName() + "님 가입 축하합니다. 쿠폰 번호: xxxxx");
     }
 
-    @EventListener
-    public void sendSMS(RegisteredUserEvent event) {
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void sendSMS(RegisteredUserEvent event) throws InterruptedException {
+        Thread.sleep(3_000);
         System.out.println(event.getName() + "님 가입 축하합니다. 문자");
     }
 }
