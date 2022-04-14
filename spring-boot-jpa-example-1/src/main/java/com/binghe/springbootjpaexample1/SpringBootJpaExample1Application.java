4acf19950d1073fc25c7f5168a95ab9e75381077
binghe819 <binghe819@gmail.com>
2022-04-14 22:02:36 +0900

    [spring-boot-jpa-example-1: init] Spring Boot + JPA 예제 1 초기 설정

---
+++ spring-boot-jpa-example-1/src/main/java/com/binghe/springbootjpaexample1/SpringBootJpaExample1Application.java
@@ -0,0 +1,13 @@
+package com.binghe.springbootjpaexample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaExample1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaExample1Application.class, args);
+    }
+
+}
