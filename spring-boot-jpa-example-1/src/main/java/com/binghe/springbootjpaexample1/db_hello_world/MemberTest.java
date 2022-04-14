5def881d3471b383b197c5d79665130f4bbe2b3f
binghe819 <binghe819@gmail.com>
2022-04-14 23:38:05 +0900

    [spring-boot-jpa-example-1: learning test] 테스트용 DB 기본 설정

---
+++ spring-boot-jpa-example-1/src/main/java/com/binghe/springbootjpaexample1/db_hello_world/MemberTest.java
@@ -0,0 +1,18 @@
+package com.binghe.springbootjpaexample1.db_hello_world;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter @Setter
+@Entity
+public class MemberTest {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+}
