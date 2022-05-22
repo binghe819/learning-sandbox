74cf297eb3847c5492ce1b2892f33da2e3bdcdfc
binghe819 <binghe819@gmail.com>
2022-05-22 19:27:33 +0900

    [jpa-query-optimization : init] JPA 조회 최적화 학습 테스트 초기 설정

---
+++ jpa-query-optimization/src/main/java/com/example/jpaqueryoptimization/JpaQueryOptimizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaqueryoptimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueryOptimizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaQueryOptimizationApplication.class, args);
+    }
+
+}
---
+++ jpa-query-optimization/src/test/java/com/example/jpaqueryoptimization/JpaQueryOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaqueryoptimization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaQueryOptimizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
