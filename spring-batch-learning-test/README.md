ad25dc15d6618a111a21995c717317630be3f959
binghe819 <binghe819@gmail.com>
2022-06-05 04:32:27 +0900

    [spring-batch-learing-test] 스프링 배치 Hello World - 디버깅용

---
+++ spring-batch-learning-test/README.md
@@ -0,0 +1,7 @@
+# Spring Batch 놀이터
+
+<br>
+
+## MySQL 설정 방법
+
+
---
+++ spring-batch-learning-test/src/main/java/com/binghe/springbatchlearningtest/HelloJobConfiguration.java
@@ -0,0 +1,47 @@
+package com.binghe.springbatchlearningtest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Batch Hello World - 디버깅용
+ */
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job helloJob() {
+        return jobBuilderFactory.get("helloJob")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(" >>> step1 실행됨");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(" >>> step2 실행됨");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
