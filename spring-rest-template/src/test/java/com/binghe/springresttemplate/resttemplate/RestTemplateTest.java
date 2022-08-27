014fb1c5f70eb4707a74ae912217d95deafe286a
binghe819 <binghe819@gmail.com>
2022-08-27 19:29:34 +0900

    [spring-rest-template : learning test] URLConnection, RestTemplate 학습 테스트

--- spring-rest-template/src/test/java/com/binghe/springresttemplate/resttemplate/RestTemplateTest.java
+++ spring-rest-template/src/test/java/com/binghe/springresttemplate/resttemplate/RestTemplateTest.java
@@ -1,2 +1,232 @@
-package com.binghe.springresttemplate.resttemplate;public class RestTemplateTest {
+package com.binghe.springresttemplate.resttemplate;
+
+
+import com.binghe.springresttemplate.user.application.UserDto;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestTemplateTest {
+    private static final String LOCAL_URL = "http://localhost:8080/users";
+
+    @Test
+    void Get_getForEntity() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        Long id = 1L;
+        String url = LOCAL_URL + "/" + id;
+
+        // when
+        ResponseEntity<UserDto> response = restTemplate.getForEntity(url, UserDto.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getId()).isEqualTo(id);
+    }
+
+    @Test
+    void Get_getForObject() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        Long id = 1L;
+        String url = LOCAL_URL + "/" + id;
+
+        // when
+        UserDto response = restTemplate.getForObject(url, UserDto.class);
+
+        // then
+        assertThat(response.getId()).isEqualTo(id);
+    }
+
+    @Test
+    void Get_exchange() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        Long id = 1L;
+        String url = LOCAL_URL + "/" + id;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(headers);
+
+        // when
+        ResponseEntity<UserDto> response = restTemplate.exchange(url, HttpMethod.GET, request, UserDto.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getId()).isEqualTo(id);
+    }
+
+    @Test
+    void Head_headForHeaders() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = LOCAL_URL;
+
+        // when
+        HttpHeaders response = restTemplate.headForHeaders(url);
+
+        // then
+        assertThat(response.getContentType().includes(MediaType.APPLICATION_JSON)).isTrue();
+    }
+
+    @Test
+    void Post_postForEntity() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = LOCAL_URL;
+        String name = "test";
+        int age = 27;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(new UserDto(null, name, age), headers);
+
+        // when
+        ResponseEntity<Long> response = restTemplate.postForEntity(url, request, Long.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isOfAnyClassIn(Long.class);
+        System.out.println(response.getBody()); // 새로 만들어진 User의 Entity ID
+    }
+
+    @Test
+    void Post_postForObject() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = LOCAL_URL;
+        String name = "test";
+        int age = 27;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(new UserDto(null, name, age), headers);
+
+        // when
+        Long response = restTemplate.postForObject(url, request, Long.class);
+
+        // then
+        assertThat(response).isNotNull();
+        System.out.println(response); // 새로 만들어진 User의 Entity ID
+    }
+
+    @Disabled
+    @Test
+    void Post_postForLocation() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = LOCAL_URL;
+        String name = "test";
+        int age = 27;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(new UserDto(null, name, age), headers);
+
+        // when
+        URI uri = restTemplate.postForLocation(url, request);
+
+        // then
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void Post_exchange() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = LOCAL_URL;
+        String name = "test";
+        int age = 27;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(new UserDto(null, name, age), headers);
+
+        // when
+        ResponseEntity<Long> response = restTemplate.exchange(url, HttpMethod.POST, request, Long.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        System.out.println(response); // 새로 만들어진 User의 Entity ID
+    }
+
+    @Test
+    void Put_put() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        String url = LOCAL_URL + "/" + 1L;
+        String updatedName = "updated_user_name";
+        int updatedAge = 27;
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(new UserDto(null, updatedName, updatedAge), headers);
+
+        // when
+        restTemplate.put(url, request);
+    }
+
+    @Test
+    void Put_exchange() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+        String url = LOCAL_URL + "/" + 1L;
+        String updatedName = "updated_user_name";
+        int updatedAge = 27;
+        HttpEntity<UserDto> request =
+                new HttpEntity<>(new UserDto(null, updatedName, updatedAge), headers);
+
+        // when
+        ResponseEntity<UserDto> response = restTemplate.exchange(url, HttpMethod.PUT, request, UserDto.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getName()).isEqualTo(updatedName);
+        assertThat(response.getBody().getAge()).isEqualTo(updatedAge);
+    }
+
+    @Test
+    void DELETE() {
+        // given
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = LOCAL_URL + "/" + 1L;
+
+        restTemplate.delete(url);
+    }
 }
--- spring-rest-template/src/test/java/com/binghe/springresttemplate/urlconnection/UrlConnectionTest.java
+++ spring-rest-template/src/test/java/com/binghe/springresttemplate/urlconnection/UrlConnectionTest.java
@@ -1,2 +1,146 @@
-package com.binghe.springresttemplate.urlconnection;public class UrlConnectionTest {
+package com.binghe.springresttemplate.urlconnection;
+
+import com.binghe.springresttemplate.user.application.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class UrlConnectionTest {
+
+    @Test
+    void Get_read_responseHeader() {
+        String url = "http://localhost:8080/users/1";
+
+        try {
+            URL urlObj = new URL(url);
+            URLConnection urlConnection = urlObj.openConnection();
+
+            // 요청 Header
+            urlConnection.setRequestProperty("User-Agent", "Java Client; Mac OS");
+            urlConnection.setRequestProperty("Accept", "*/*");
+            urlConnection.setRequestProperty("Connection", "keep-alive");
+
+            // 응답 Header 가져오기
+            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+
+            headerFields.forEach((key, value) -> {
+                System.out.println(key + " : " + value);
+            });
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    void Get_read_responseBody() {
+        String url = "http://localhost:8080/users/1";
+
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+
+            // 요청 Header
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoInput(true);
+            urlConnection.setRequestProperty("User-Agent", "Java Client; Mac OS");
+            urlConnection.setRequestProperty("Accept", "*/*");
+            urlConnection.setRequestProperty("Connection", "keep-alive");
+
+            // 응답 가져오기
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder responseBody = new StringBuilder();
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            // 연결 종료
+            reader.close();
+
+            // 출력
+            System.out.println(responseBody);
+        } catch (MalformedURLException e) {
+            System.out.println("URL 객체에 URL 주소를 잘못주면 발생하는 예외");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("openConnection는 입력 혹은 출력 스트림에서 예외가 발생하면 IOException을 던진다.");
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void Post_send_requestBody_and_read_responseBody() {
+        String url = "http://localhost:8080/users";
+
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+
+            // 요청 Header
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoInput(true);
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestProperty("User-Agent", "Java Client; Mac OS");
+            urlConnection.setRequestProperty("Accept", "*/*");
+            urlConnection.setRequestProperty("Content-Type", "application/json; utf-8");
+            urlConnection.setRequestProperty("Connection", "keep-alive");
+
+            // 요청 Body
+            String requestBody = "{\"name\":\"testtest15\",\"age\":27}";
+            try (OutputStream os = urlConnection.getOutputStream()){
+                byte request_data[] = requestBody.getBytes("utf-8");
+                os.write(request_data);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            // 연결 (HTTP 연결 실시)
+            urlConnection.connect();
+
+            // 응답 헤더 출력
+            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+
+            System.out.println("상태 코드: " + urlConnection.getResponseCode());
+            headerFields.forEach((key, value) -> {
+                System.out.println(key + " : " + value);
+            });
+
+            // 응답 바디 출력
+            InputStream inputStream = urlConnection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
+
+            StringBuilder responseBody = new StringBuilder();
+            String line = "";
+            while((line = reader.readLine()) != null) {
+                responseBody.append(line);
+            }
+            reader.close();
+
+            // 응답 바디 출력
+            System.out.println(responseBody);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
