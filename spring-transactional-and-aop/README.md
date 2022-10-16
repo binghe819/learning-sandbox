51dd30d23e0b0b5ee825d485cb3ce9f52691b76c
binghe819 <binghe819@gmail.com>
2022-10-16 16:39:57 +0900

    [spring-transactional-and-aop] init project

---
+++ spring-transactional-and-aop/README.md
@@ -0,0 +1,4 @@
+# 트랜잭션 예제로 알아보는 스프링 @Transactional과 AOP
+
+
+
---
+++ spring-transactional-and-aop/src/main/java/com/binghe/springtransactionalandaop/SpringTransactionalAndAopApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.springtransactionalandaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionalAndAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionalAndAopApplication.class, args);
+    }
+
+}
