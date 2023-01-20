9076e3451005f70fa3c7b495d4a3a8e442698abc
binghe819 <binghe819@gmail.com>
2023-01-20 21:05:33 +0900

    [inflearn-spring-aop : init] init project

---
+++ inflearn-spring-aop/src/main/java/com/binghe/inflearnspringaop/InflearnSpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.inflearnspringaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringAopApplication.class, args);
+    }
+
+}
---
+++ inflearn-spring-aop/src/test/java/com/binghe/inflearnspringaop/InflearnSpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.inflearnspringaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
