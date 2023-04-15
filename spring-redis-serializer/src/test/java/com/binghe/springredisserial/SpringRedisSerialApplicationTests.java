5674bfeec3f855c73e261ce4b32d7cbd0b3e88a2
binghe819 <binghe819@gmail.com>
2023-04-16 01:17:39 +0900

    [spring-redis-serializer] init project

---
+++ spring-redis-serializer/src/test/java/com/binghe/springredisserial/SpringRedisSerialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.springredisserial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisSerialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
