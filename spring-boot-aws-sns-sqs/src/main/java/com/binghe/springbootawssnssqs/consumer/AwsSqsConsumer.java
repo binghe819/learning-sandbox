ad6f4b830b58e5f2cb7d933fbfb4d0c9b4445225
binghe819 <binghe819@gmail.com>
2023-07-27 19:45:46 +0900

    spring sqs sns : sqs event deletion policy

--- spring-boot-aws-sns-sqs/src/main/java/com/binghe/springbootawssnssqs/consumer/AwsSqsConsumer.java
+++ spring-boot-aws-sns-sqs/src/main/java/com/binghe/springbootawssnssqs/consumer/AwsSqsConsumer.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class AwsSqsConsumer {
 
     @SqsListener(value = "${sqs-event.binghe-test-sqs}", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    public void consume(EventMessageSample event, Acknowledgment ack) {
+    public void consume(EventMessageSample event) {
         log.info("Consumed Message : {}", event.toString());
 
 //        SqsMessageDeletionPolicy.NEVER 설정시 명시적으로 아래와 같이 메시지를 삭제하도록 ack 응답을 보낼 수 있다.
