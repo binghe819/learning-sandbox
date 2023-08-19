67c5516990019b7850318aa0f3ce13c4af12fd1d
binghe819 <binghe819@gmail.com>
2023-08-20 03:28:16 +0900

    inflearn-spring-core-basic: init project

---
+++ inflearn-spring-core-basic/src/main/java/com/binghe/inflearnspringcorebasic/InflearnSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.inflearnspringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringCoreBasicApplication.class, args);
+    }
+
+}
---
+++ inflearn-spring-core-basic/src/test/java/com/binghe/inflearnspringcorebasic/InflearnSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.inflearnspringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
