264f2084f1ed6b4f2be472a546612d61c4470abb
binghe819 <binghe819@gmail.com>
2023-11-12 16:03:48 +0900

    init spring-cloud-openfeign

---
+++ spring-cloud-openfeign/README.md
@@ -0,0 +1,14 @@
+# Spring-Cloud OpenFeign
+
+<br>
+
+# 의존성
+* https://spring.io/projects/spring-cloud
+* https://isntyet.github.io/java/feign-client-%EC%82%AC%EC%9A%A9%ED%95%B4%EB%B3%B4%EA%B8%B0/
+
+<br>
+
+<br>
+
+# 
+
---
+++ spring-cloud-openfeign/src/main/java/com/binghe/springcloudopenfeign/SpringCloudOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.springcloudopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudOpenfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudOpenfeignApplication.class, args);
+    }
+
+}
