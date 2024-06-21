0fd095e145246520d6ee43d7bab81a79c9193553
김병화 <binghe819@woowahan.com>
2024-06-21 12:01:59 +0900

    java-web-performance-tomcat

---
+++ java-web-performance-tomcat/src/main/java/com/binghe/javawebperformancetomcat/JavaWebPerformanceTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.binghe.javawebperformancetomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebPerformanceTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebPerformanceTomcatApplication.class, args);
+    }
+
+}
---
+++ java-web-performance-tomcat/src/test/java/com/binghe/javawebperformancetomcat/JavaWebPerformanceTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.javawebperformancetomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebPerformanceTomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
