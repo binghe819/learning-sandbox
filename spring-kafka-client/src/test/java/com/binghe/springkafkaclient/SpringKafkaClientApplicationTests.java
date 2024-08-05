0f2a0b163166ea1d13d18c3ee3e554fab829947e
김병화 <binghe819@woowahan.com>
2024-08-05 10:47:25 +0900

    spring kafka init

---
+++ spring-kafka-client/src/test/java/com/binghe/springkafkaclient/SpringKafkaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binghe.springkafkaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
