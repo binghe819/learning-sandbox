06430a0fae0059f1b1430f836eb843d63e65abd2
김병화 <binghe819@woowahan.com>
2024-10-03 22:41:43 +0900

    kotlin-grpc-coroutine-spring-boot - module

--- kotlin-grpc-coroutine-spring-boot/settings.gradle.kts
+++ kotlin-grpc-coroutine-spring-boot/settings.gradle.kts
@@ -2,4 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "kotlin-grpc-coroutine-spring-boot"
-
+include("untitled")
+include("grpc-protocol")
+include("grpc-client")
+include("grpc-server")
