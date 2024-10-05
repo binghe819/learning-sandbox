08139f5996500a066ec2af538c933f3d59f2393e
김병화 <binghe819@woowahan.com>
2024-10-05 18:29:32 +0900

    performance test db schema

--- java-web-performance-springmvc/src/main/resources/schema.sql
+++ java-web-performance-springmvc/src/main/resources/schema.sql
@@ -3,7 +3,7 @@ create table members
     id          int auto_increment,
     name        varchar(500) not null,
     age         int          null,
-    description text         null,
+    description mediumtext    null,
     constraint members_pk
         primary key (id)
 );
