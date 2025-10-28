3402914f4c180a5ec0465de786eeb327b73accd4
김병화 <binghe819@woowahan.com>
2025-10-29 02:43:34 +0900

    thrift-netty-sandbox: init

---
+++ thrift-netty-sandbox/thrift-client/src/main/java/com/binghe/ThriftClientApplication.java
@@ -0,0 +1,8 @@
+package com.binghe;
+
+public class ThriftClientApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Thrift Client");
+    }
+}
---
+++ thrift-netty-sandbox/thrift-server/src/main/java/com/binghe/ThriftServerApplication.java
@@ -0,0 +1,8 @@
+package com.binghe;
+
+public class ThriftServerApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello thrift server");
+    }
+}
